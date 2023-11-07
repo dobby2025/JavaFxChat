20b23d39914084a6e67f7969e367f5b5cb42e81a
dobby2025 <128953898+dobby2025@users.noreply.github.com>
2023-11-07 21:41:57 +0900

    javafx

---
+++ src/application/MainTest.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.UUID;
+
+public class MainTest {
+	public static void main(String[] args) {
+		UUID uniqueKey = UUID.randomUUID();
+		
+		System.out.println(uniqueKey);
+	}
+
+}
